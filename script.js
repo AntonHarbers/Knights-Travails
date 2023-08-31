ad90ac74b85cc824718ab10cc5a074e7f5632f2d
tmonee23 <33356106+tmonee23@users.noreply.github.com>
2023-08-31 22:57:53 +0400

    Feat: Board and Knight class implementation started

--- script.js
+++ script.js
@@ -1,19 +1,75 @@
-console.log("Hello World!");
+// GLOBAL VARIABLES:
 
-class KnightsTravails{
-    constructor(start, end) {
+const boardDiv = document.getElementById('board');
+
+// CLASSES:
+
+class Board{
+    constructor(){
         this.board = [];
+    }
+
+    createBoard(){
+        for(let i = 0; i < 8; i++){
+            this.board.push([]);
+            for(let j = 0; j < 8; j++){
+                this.board[i].push(0);
+            }
+        }
+    }
+
+    renderBoard(){
+        for(let i = 0; i < this.board.length; i++){
+            for(let j = 0; j < this.board[i].length; j++){
+                const square = document.createElement('div');
+                square.setAttribute('id', `${i}${j}`);
+                square.classList.add('square');
+                // set dark or white square
+                if((i + j) % 2 === 0){
+                    square.classList.add('black');
+                } else {
+                    square.classList.add('white');
+                }
+                boardDiv.appendChild(square);
+            }
+        }
+    }
+
+}
+
+class Knight{
+    constructor() {
         this.visited = [];
         this.queue = [];
         this.path = [];
-        this.start = start;
-        this.end = end;
+        this._start = [null, null];
+        this._end = [null, null];
     }
 
-    printStart(){
-        console.log(this.start);
+    get start(){
         return this.start;
+    }
+
+    get end(){
+        return this.end;
+    }
+
+    set start(start){
+        this._start = start;
+    }
+
+    set end(end){
+        this._end = end;
     }
 }
 
-module.exports = {KnightsTravails};
+// FUNCTIONALITY:
+
+const boardInstance = new Board();
+boardInstance.createBoard();
+boardInstance.renderBoard();
+
+const knightInstance = new Knight();
+knightInstance.start = [1,1]
+
+
